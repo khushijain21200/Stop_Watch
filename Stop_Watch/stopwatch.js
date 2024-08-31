14bc1d898150029b44f14ccafd3653e5d5daa444
Khushi Jain <107362355+khushijain21200@users.noreply.github.com>
2024-09-01 02:06:19 +0530

    Add files via upload

---
+++ Stop_Watch/stopwatch.js
@@ -0,0 +1,93 @@
+let startBtn = document.getElementById('start');
+let stopBtn = document.getElementById('stop');
+let resetBtn = document.getElementById('reset');
+
+let hour = 11;
+let minute = 59;
+let second =55;
+let count = 0;
+
+startBtn.addEventListener('click', function () {
+	timer = true;
+	stopWatch();
+});
+
+stopBtn.addEventListener('click', function () {
+	timer = false;
+});
+
+resetBtn.addEventListener('click', function () {
+	timer = false;
+	hour = 0;
+	minute = 0;
+	second = 0;
+	count = 0;
+	document.getElementById('hr').innerHTML = "00";
+	document.getElementById('min').innerHTML = "00";
+	document.getElementById('sec').innerHTML = "00";
+	document.getElementById('count').innerHTML = "00";
+});
+
+function stopWatch() {
+	if (timer) 
+	{
+		count++;
+
+		if (count == 100) 
+		{
+			second++;
+			count = 0;
+		}
+
+		if (second == 60) 
+		{
+			minute++;
+			second = 0;
+		}
+
+		if (minute == 60) 
+		{
+			hour++;
+			minute = 0;
+			second = 0;
+		}
+
+		let hrString = hour;
+		let minString = minute;
+		let secString = second;
+		let countString = count;
+		let AMPM="AM"
+
+		if (hour < 10) 
+		{
+			hrString = "0" + hrString;
+		}
+
+		if(hour>=12)
+		{
+			AMPM="PM"
+		}
+
+		if (minute < 10) 
+		{
+			minString = "0" + minString;
+		}
+
+		if (second < 10) 
+		{
+			secString = "0" + secString;
+		}
+
+		if (count < 10) 
+		{
+			countString = "0" + countString;
+		}
+
+		document.getElementById('hr').innerHTML = hrString;
+		document.getElementById('min').innerHTML = minString;
+		document.getElementById('sec').innerHTML = secString;
+		document.getElementById('count').innerHTML = countString;
+		document.getElementById("AMPM").innerText=AMPM;
+		setTimeout(stopWatch, 10);
+	}
+}
